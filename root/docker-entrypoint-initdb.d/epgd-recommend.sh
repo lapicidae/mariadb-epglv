6a7b59c8fdeab22440de46b99c45c2fffb7dba08
Andreas Hemmerle <andreas.hemmerle@gmail.com>
2020-04-19 22:29:10 +0200

    bugfix

--- root/docker-entrypoint-initdb.d/epgd-recommend.sh
+++ root/docker-entrypoint-initdb.d/epgd-recommend.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 cnf="/etc/mysql/conf.d/mysqlepgd.cnf"
 
 if [ "$EPGD_RECOMMEND" = "yes" ]; then
+	echo ">>>>>>>>>> epgd recommend settings to $cnf <<<<<<<<<<"
 	echo -e "[mysqld]\nmax_allowed_packet=128M\nwait_timeout=86400\nconnect_timeout=600\ninteractive_timeout=86400\nnet_read_timeout=600\nnet_write_timeout=180\ninnodb_lock_wait_timeout=100\ninnodb_rollback_on_timeout=1" > $cnf
 else
 	if [ -f "$cnf" ]; then
