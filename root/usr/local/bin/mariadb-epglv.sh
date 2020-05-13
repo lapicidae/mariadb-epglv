ae9fa3a2ff50e275c8e1abb38e1fee45849296c6
lapicidae <andreas.hemmerle@gmail.com>
2020-05-13 22:06:30 +0200

    cosmetics

--- root/usr/local/bin/mariadb-epglv.sh
+++ root/usr/local/bin/mariadb-epglv.sh
@@ -28,16 +28,16 @@ if [ "$EPGD_RECOMMEND" = "yes" ]; then
 	echo ">>>>>>>>>> pushing epgd recommend settings to $cnf <<<<<<<<<<"
 	cat <<- EOF > $cnf
 	[mysqld]
-	log_warnings=$MARIADB_LOG_WARNINGS
-	max_allowed_packet=$MARIADB_MAX_ALLOWED_PACKET
-	wait_timeout=$MARIADB_WAIT_TIMEOUT
 	connect_timeout=$MARIADB_CONNECT_TIMEOUT
-	interactive_timeout=$MARIADB_INTERACTIVE_TIMEOUT
-	net_read_timeout=$MARIADB_NET_READ_TIMEOUT
-	net_write_timeout=$MARIADB_NET_WRITE_TIMEOUT
 	innodb_lock_wait_timeout=$MARIADB_INNODB_LOCK_WAIT_TIMEOUT
 	innodb_rollback_on_timeout=$MARIADB_INNODB_ROLLBACK_ON_TIMEOUT
+	interactive_timeout=$MARIADB_INTERACTIVE_TIMEOUT
+	log_warnings=$MARIADB_LOG_WARNINGS
+	max_allowed_packet=$MARIADB_MAX_ALLOWED_PACKET
+	net_read_timeout=$MARIADB_NET_READ_TIMEOUT
+	net_write_timeout=$MARIADB_NET_WRITE_TIMEOUT
 	transaction-isolation=$MARIADB_TRANSACTION_ISOLATION
+	wait_timeout=$MARIADB_WAIT_TIMEOUT
 	EOF
 else
 	if [ -f "$cnf" ]; then
