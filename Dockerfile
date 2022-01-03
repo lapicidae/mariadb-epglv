89d5b3b40a63641efc6a158b96632c0db6608064
lapicidae <andreas.hemmerle@gmail.com>
2022-01-04 00:00:21 +0100

    rework recommended settings

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ RUN sed -i 'H;1h;$!d;G' /etc/apt/sources.list.d/mariadb.list && \
     sed -i '2s/deb/deb-src/' /etc/apt/sources.list.d/mariadb.list && \
     apt-get update -qq && \
     apt-get install -qy apt-utils && \
-    echo "**** timezone and locale ****" && \
-    apt-get install -qy locales &&\
-    echo "Europe/Berlin" > /etc/timezone &&\
-    dpkg-reconfigure -f noninteractive tzdata &&\
-    echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen; locale-gen && \
     echo "**** install build packages ****" && \
     DEBIAN_FRONTEND=noninteractive apt-get install -qy \
       build-essential \
@@ -52,22 +47,6 @@ RUN sed -i 'H;1h;$!d;G' /etc/apt/sources.list.d/mariadb.list && \
     chmod 755 /usr/local/bin/mariadb-epglv.sh && \
     ln -s /usr/local/bin/mariadb-epglv.sh /mariadb-epglv.sh
 
-ENV LANG="de_DE.UTF-8" \
-    LANGUAGE="de_DE:de" \
-    LC_ALL="de_DE.UTF-8" \
-    EPGD_RECOMMEND="yes" \
-    MARIADB_CONNECT_TIMEOUT="300" \
-    MARIADB_INNODB_DEFRAGMENT="1" \
-    MARIADB_INNODB_LOCK_WAIT_TIMEOUT="600" \
-    MARIADB_INNODB_ROLLBACK_ON_TIMEOUT="0" \
-    MARIADB_INTERACTIVE_TIMEOUT="86400" \
-    MARIADB_LOG_WARNINGS="2" \
-    MARIADB_MAX_ALLOWED_PACKET="128M" \
-    MARIADB_NET_READ_TIMEOUT="600" \
-    MARIADB_NET_WRITE_TIMEOUT="300" \
-    MARIADB_TABLE_DEFINITION_CACHE="1200" \
-    MARIADB_TABLE_OPEN_CACHE="1200" \
-    MARIADB_TRANSACTION_ISOLATION="READ-COMMITTED" \
-    MARIADB_WAIT_TIMEOUT="86400"
+ENV EPGD_RECOMMEND="true"
 
 ENTRYPOINT ["/mariadb-epglv.sh"]
--- root/usr/local/bin/mariadb-epglv.sh
+++ root/usr/local/bin/mariadb-epglv.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cnf="/etc/mysql/conf.d/mysqlepgd.cnf"
-
 echo '
 --------------------------------------------------------------
         __  __    __    ____  ____    __    ____  ____
@@ -24,34 +22,54 @@ User gid:    $(id -g mysql)
 --------------------------------------------------------------
 "
 
-if [ "$EPGD_RECOMMEND" = "yes" ]; then
+cnf="/etc/mysql/conf.d/mysqlepgd.cnf"
+
+## defaults
+epglv_connect_timeout=${RCMD_CONNECT_TIMEOUT:-"300"}
+epglv_innodb_defragment=${RCMD_INNODB_DEFRAGMENT:-"1"}
+epglv_innodb_lock_wait_timeout=${RCMD_INNODB_LOCK_WAIT_TIMEOUT:-"600"}
+epglv_innodb_rollback_on_timeout=${RCMD_INNODB_ROLLBACK_ON_TIMEOUT:-"0"}
+epglv_interactive_timeout=${RCMD_INTERACTIVE_TIMEOUT:-"86400"}
+epglv_log_warnings=${RCMD_LOG_WARNINGS:-"2"}
+epglv_max_allowed_packet=${RCMD_MAX_ALLOWED_PACKET:-"128M"}
+epglv_net_read_timeout=${RCMD_NET_READ_TIMEOUT:-"600"}
+epglv_net_write_timeout=${RCMD_NET_WRITE_TIMEOUT:-"300"}
+epglv_table_definition_cache=${RCMD_TABLE_DEFINITION_CACHE:-"1200"}
+epglv_table_open_cache=${RCMD_TABLE_OPEN_CACHE:-"1200"}
+epglv_transaction_isolation=${RCMD_TRANSACTION_ISOLATION:-"READ-COMMITTED"}
+epglv_wait_timeout=${RCMD_WAIT_TIMEOUT:-"86400"}
+
+
+if [ "$EPGD_RECOMMEND" != "false" ]; then
 	echo ">>>>>>>>>> pushing epgd recommend settings to $cnf <<<<<<<<<<"
 	cat <<- EOF > $cnf
-	[mysqld]
-	performance_schema=ON
-	innodb_stats_on_metadata=0
-	connect_timeout=$MARIADB_CONNECT_TIMEOUT
-	innodb_defragment=$MARIADB_INNODB_DEFRAGMENT
-	innodb_lock_wait_timeout=$MARIADB_INNODB_LOCK_WAIT_TIMEOUT
-	innodb_rollback_on_timeout=$MARIADB_INNODB_ROLLBACK_ON_TIMEOUT
-	interactive_timeout=$MARIADB_INTERACTIVE_TIMEOUT
-	log_warnings=$MARIADB_LOG_WARNINGS
-	max_allowed_packet=$MARIADB_MAX_ALLOWED_PACKET
-	net_read_timeout=$MARIADB_NET_READ_TIMEOUT
-	net_write_timeout=$MARIADB_NET_WRITE_TIMEOUT
-	table_definition_cache = $MARIADB_TABLE_DEFINITION_CACHE
-	table_open_cache = $MARIADB_TABLE_OPEN_CACHE
-	transaction-isolation=$MARIADB_TRANSACTION_ISOLATION
-	wait_timeout=$MARIADB_WAIT_TIMEOUT
+	[mariadb]
+	connect_timeout=$epglv_connect_timeout
+	innodb_defragment=$epglv_innodb_defragment
+	innodb_lock_wait_timeout=$epglv_innodb_lock_wait_timeout
+	innodb_rollback_on_timeout=$epglv_innodb_rollback_on_timeout
+	interactive_timeout=$epglv_interactive_timeout
+	log_warnings=$epglv_log_warnings
+	max_allowed_packet=$epglv_max_allowed_packet
+	net_read_timeout=$epglv_net_read_timeout
+	net_write_timeout=$epglv_net_write_timeout
+	table_definition_cache = $epglv_table_definition_cache
+	table_open_cache = $epglv_table_open_cache
+	transaction-isolation=$epglv_transaction_isolation
+	wait_timeout=$epglv_wait_timeout
 	EOF
 
-	if [ -z "$MARIADB_INNODB_BUFFER_POOL_SIZE" ]; then
-		MARIADB_INNODB_BUFFER_POOL_SIZE=$(awk '/^Mem/ {print($2*75/100);}' <(free --bytes))			# 75% of available RAM
-		echo "innodb_buffer_pool_size=$MARIADB_INNODB_BUFFER_POOL_SIZE" >> $cnf
+	if [ -z "$RCMD_INNODB_BUFFER_POOL_SIZE" ]; then
+		epglv_innodb_buffer_pool_size=$(awk '/^Mem/ {print($2*75/100);}' <(free --bytes))			# 75% of available RAM
+		if [[ "$epglv_innodb_buffer_pool_size" =~ ^[0-9]+$ ]]; then	# if var is an integer
+			echo "innodb_buffer_pool_size=$epglv_innodb_buffer_pool_size" >> $cnf
+		fi
 	fi
-	if [ -z "$MARIADB_INNODB_LOG_FILE_SIZE" ]; then
-		MARIADB_INNODB_LOG_FILE_SIZE=$(awk '{print($1*25/100);}' <(echo $MARIADB_INNODB_BUFFER_POOL_SIZE))	# 25% of buffer pool size
-		echo "innodb_log_file_size=$MARIADB_INNODB_LOG_FILE_SIZE" >> $cnf
+	if [ -z "$RCMD_INNODB_LOG_FILE_SIZE" ] && [ ! -z "$epglv_innodb_buffer_pool_size" ]; then
+		epglv_innodb_log_file_size=$(awk '{print($1*25/100);}' <(echo $epglv_innodb_buffer_pool_size))		# 25% of buffer pool size
+		if [[ "$epglv_innodb_log_file_size" =~ ^[0-9]+$ ]]; then	# if var is an integer
+			echo "innodb_log_file_size=$epglv_innodb_log_file_size" >> $cnf
+		fi
 	fi
 else
 	if [ -f "$cnf" ]; then
