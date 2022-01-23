221424ec6ddef76fa8b93147bcd260486503deb2
lapicidae <andreas.hemmerle@gmail.com>
2022-01-23 09:07:43 +0100

    first run: set svg as logoSuffix for epghttpd

--- root/docker-entrypoint-initdb.d/mysql-first-time.sql
+++ root/docker-entrypoint-initdb.d/mysql-first-time.sql
@@ -4,6 +4,25 @@ GRANT ALL PRIVILEGES ON epg2vdr.* TO 'epg2vdr'@'%' IDENTIFIED BY 'epg';
 GRANT ALL PRIVILEGES ON epg2vdr.* TO 'epg2vdr'@'localhost' IDENTIFIED BY 'epg';
 FLUSH PRIVILEGES;
 
+-- set svg as logoSuffix for epghttpd
+USE epg2vdr;
+
+CREATE TABLE IF NOT EXISTS `parameters` (
+  `owner` varchar(40) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `inssp` int(11) DEFAULT NULL,
+  `updsp` int(11) DEFAULT NULL,
+  `value` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+INSERT INTO `parameters` (`owner`, `name`, `inssp`, `updsp`, `value`) VALUES
+('epgd', 'logoSuffix', 0, 0, 'svg');
+
+ALTER TABLE `parameters`
+  ADD PRIMARY KEY (`owner`,`name`);
+COMMIT;
+
+-- create functions
 DROP FUNCTION IF EXISTS epglv;
 DROP FUNCTION IF EXISTS epglvr;
 CREATE FUNCTION epglv RETURNS INT SONAME 'mysqlepglv.so';
